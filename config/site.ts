3394ff0002d123fe6405b021e0f5805854448aba
Matheus Mazeto <nb27582@novabase.pt>
2023-08-02 18:02:56 +0100

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "matheusmazeto.com",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/matheusmazeto",
+    github: "https://github.com/matheusmazeto",
+  },
+}
