2a185e088d94d179301bb5495b3192fe353479ba
Matheus Mazeto <nb27582@novabase.pt>
2023-08-05 17:34:13 +0100

    chore: fix terminal warnings with content layer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from "next-contentlayer"
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,4 +8,4 @@ const nextConfig = {
   },
 }
 
-export default withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
